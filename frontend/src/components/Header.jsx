dd079e38f2e38c9f59b584a6113246104fda668d
Dhruv Pal <dhruvpal.4818@gmail.com>
2025-02-18 02:26:33 +0530

    add speciality section

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,22 +3,27 @@ import { assets } from "../assets/assets";
 
 const Header = () => {
   return (
-    <div className="flex flex-col md:flex-row flex-wrap bg-primary rounded-lg px-6 md:px-10 lg:px-20">
+    <div className="flex flex-col md:flex-row flex-wrap bg-primary rounded-lg px-6 mt-4 md:px-10 lg:px-20">
       {/* ---------- Left side ------------ */}
       <div className="md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]">
         <p className="text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight">
           Book Appointment <br /> With Trusted Doctors
         </p>
-        <div>
-          <img src={assets.group_profiles} alt="" />
+        <div className="flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light">
+          <img src={assets.group_profiles} alt="" className="w-24" />
           <p>
-            Simply browse through our extensive list of trusted doctors, <br />
+            Simply browse through our extensive list of trusted doctors,{" "}
+            <br className="hidden sm:block" />
             schedule your appointment hassle-free.
           </p>
-          <a href="#">
-            Book appointment <img src={assets.arrow_icon} alt="" />
-          </a>
         </div>
+        <a
+          href="#speciality"
+          className="flex bg-white items-center gap-2 px-8 py-3 text-gray-600 rounded-full text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300"
+        >
+          Book appointment{" "}
+          <img src={assets.arrow_icon} alt="" className="w-3" />
+        </a>
       </div>
       {/* ---------- right side ------------ */}
       <div className="md:w-1/2 relative ">
