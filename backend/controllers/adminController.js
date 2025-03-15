b7efc3c88c875cdc1766ef88bae7dc0936b6bed1
Dhruv Pal <dhruvpal.4818@gmail.com>
2025-03-16 00:47:28 +0530

    add new doctor data save in db

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,3 +1,8 @@
+import validator from "validator";
+import bcrypt, { hash } from "bcryptjs";
+import { v2 as cloudinary } from "cloudinary";
+import doctorModel from "../models/doctorModel.js";
+
 // API for adding doctor
 const addDoctor = async (req, res) => {
   try {
@@ -5,11 +10,12 @@ const addDoctor = async (req, res) => {
       name,
       email,
       password,
-      speciality,
       degree,
       experience,
+      about,
       fees,
       address,
+      speciality,
     } = req.body;
     const imageFile = req.file;
 
@@ -19,6 +25,7 @@ const addDoctor = async (req, res) => {
       !email ||
       !password ||
       !speciality ||
+      !about ||
       !degree ||
       !experience ||
       !fees ||
@@ -28,7 +35,54 @@ const addDoctor = async (req, res) => {
     }
 
     // validating email format
-  } catch (err) {}
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "Please enter a valid email address",
+      });
+    }
+
+    // validating strong password
+    if (password.lenght < 8) {
+      return res.json({
+        success: false,
+        message: "Please enter a strong password",
+      });
+    }
+
+    // hashing doctor password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // upload image to cloudinary to get url for image
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+      resource_type: "image",
+    });
+    const imageUrl = imageUpload.secure_url;
+
+    // save to db
+    const doctorData = {
+      name,
+      email,
+      image: imageUrl,
+      password: hashedPassword,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address: JSON.parse(address),
+      date: Date.now(),
+    };
+
+    const newDoctor = new doctorModel(doctorData);
+    await newDoctor.save();
+
+    res.json({ success: true, message: "Doctor Added" });
+  } catch (err) {
+    console.log(err);
+    res.json({ success: false, message: err.message });
+  }
 };
 
 export { addDoctor };
--- backend/models/doctorModel.js
+++ backend/models/doctorModel.js
@@ -38,7 +38,7 @@ const doctorSchema = new mongoose.Schema(
     },
     available: {
       type: Boolean,
-      required: true,
+      default: true,
     },
     fees: {
       type: Number,
